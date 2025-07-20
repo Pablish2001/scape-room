c57511dd5d2ab2634dbcc19a7ef87b636d8862b7
Pablish <prjr2001@gmail.com>
2025-07-20 15:17:13 -0300

    [update] ajustar movilidad y asignarles animacion

--- area_2d.gd
+++ area_2d.gd
@@ -2,24 +2,30 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var input_vector = Vector2.ZERO
+	input_vector.y = Input.get_axis("ui_up","ui_down")
+	input_vector.x = Input.get_axis("ui_left", "ui_right")
+	input_vector = input_vector.normalized()
+	
+	if input_vector:
+		velocity = input_vector * SPEED
+		if abs(input_vector.x) == abs(input_vector.y) || abs(input_vector.x) > abs(input_vector.y):
+			sprite.animation = "walk"
+			sprite.flip_h = input_vector.x > 0
+		else:
+			if input_vector.y > 0:
+				sprite.animation="down_walk"
+			else:
+				sprite.animation="up_walk"
+		sprite.play()	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		sprite.animation = "idle"
+		sprite.play()
 
 	move_and_slide()
